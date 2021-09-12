6cfa38fe56ade6b052c3354505bd8447f1d5ceb8
Byron Cole Lawlis <bclaw8300@gmail.com>
2021-09-12 13:18:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <div class="article">
                 <h2><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-                    <?php the_content(); ?> 
+                    <?php the_excerpt(); ?> 
             </div>
             <?php endwhile; else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
